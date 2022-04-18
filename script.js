e24a2da575b2d5bdd42b4cf7bc5b8abeb51a7fb5
Rafał <63006574+rafalrab@users.noreply.github.com>
2022-04-19 00:55:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 'use strict';
 
 // Selecting elements
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn-roll');
+const btnHold = document.querySelector('.btn--hold');
 let roll = 0;
 
+// start conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+// rolling dice functionallity
 document.querySelector('.btn--roll').addEventListener('click', function () {
-    roll = Math.trunc(Math.random() * 6) + 1;
-    console.log(roll);
-    if(roll === '1') {}
+  roll = Math.trunc(Math.random() * 6) + 1;
+  console.log(roll);
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${roll}.png`;
+
+  if (roll !== 1) {
+    currentScore += roll;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch to enxt player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
